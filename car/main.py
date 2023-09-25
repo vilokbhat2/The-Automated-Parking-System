eebfd49b49a0132b9c2092a1e5175416b71251c1
vilokbhat2 <127276554+vilokbhat2@users.noreply.github.com>
2023-09-25 18:26:53 +0530

    main.py

--- car/main.py
+++ car/main.py
@@ -5,25 +5,18 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QPushB
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
-
-        # Create UI elements
         self.setWindowTitle("Car Parking Management")
         self.setGeometry(100, 100, 400, 200)
-
         self.label = QLabel("Car Number:", self)
         self.label.move(20, 30)
-
         self.car_number_input = QLineEdit(self)
         self.car_number_input.move(120, 30)
-
         self.entry_button = QPushButton("Entry", self)
         self.entry_button.move(120, 70)
         self.entry_button.clicked.connect(self.entry)
-
         self.exit_button = QPushButton("Exit", self)
         self.exit_button.move(220, 70)
         self.exit_button.clicked.connect(self.exit)
-
         self.result_label = QLabel(self)
         self.result_label.move(120, 110)
         self.result_label.setText("Result will be displayed here.")
